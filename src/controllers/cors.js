f192d48c180c29974d645b8656829d1a2eafe9fa
Andreysson Araujo <101846961+Andreysson-Araujo@users.noreply.github.com>
2023-03-21 16:10:21 -0500

    Update cors.js

--- src/controllers/cors.js
+++ src/controllers/cors.js
@@ -5,7 +5,7 @@ function corsMiddleware(req, res, next) {
       res.setHeader("Access-Control-Allow-Origin", origin);
     }
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTION");
     next();
   }
   
